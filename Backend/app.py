a5e05b9d2fda54849996dfa63ed22192c2666ca4
AbhayKumarVishwakarma <3492akv@gmail.com>
2023-07-19 18:48:22 +0530

    done all api

--- Backend/app.py
+++ Backend/app.py
@@ -3,7 +3,7 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-mongo_uri = 'mongo_url'
+mongo_uri = 'mongodb+srv://root:root@cluster0.tktlftp.mongodb.net/?retryWrites=true&w=majority'
 
 client = MongoClient(mongo_uri)
 db = client.Mongo
@@ -78,6 +78,222 @@ def delete_portfolio_manager(manager_id):
 
     db.manager.delete_one({'manager_id': manager_id})
     return jsonify({'message': f'Manager with id: {manager_id} is deleted!'})
+
+
+# ---- Project ----
+@app.route('/project', methods=['POST'])
+def create_project():
+    data = request.get_json()
+
+    if 'project_id' not in data or 'project_name' not in data or 'status' not in data or \
+            'start_date' not in data or 'end_date' not in data:
+        return jsonify({'message': 'Error, Invalid data'}), 400
+
+    db.project.insert_one(data)
+    return jsonify({'message': 'New project created successfully!'})
+
+
+@app.route('/project/<int:project_id>', methods=['PUT'])
+def update_project(project_id):
+    project = db.project.find_one({'project_id': project_id})
+
+    if project is None:
+        return jsonify({'message': f'Not find any project with id: {project_id}!'}), 404
+
+    data = request.get_json()
+    if 'manager_id' in data: project['manager_id'] = data['manager_id']
+    if 'status' in data: project['status'] = data['status']
+    if 'end_date' in data: project['end_date'] = data['end_date']
+
+    db.project.delete_one({'project_id': project_id})
+    db.project.insert_one(project)
+
+    return jsonify({'message': f'Project with id: {project_id} is updated!'})
+
+
+@app.route('/project/<int:project_id>', methods=['DELETE'])
+def delete_project(project_id):
+    project = db.project.find_one({'project_id': project_id})
+
+    if project is None:
+        return jsonify({'message': f'Not find any project with id: {project_id}!'}), 404
+
+    db.project.delete_one({'project_id': project_id})
+    return jsonify({'message': f'Project with id: {project_id} is deleted!'})
+
+
+@app.route('/projects', methods=['GET'])
+def view_all_project():
+    projects = list(db.project.find({}))
+
+    if len(projects) == 0:
+        return jsonify({'message': 'Not find any project!'}), 404
+
+    for project in projects:
+        project['_id'] = str(project['_id'])
+
+    return jsonify(projects)
+
+
+@app.route('/project/<int:project_id>', methods=['GET'])
+def view_project(project_id):
+    project = db.project.find_one({'project_id': project_id})
+
+    if project is None:
+        return jsonify({f'message': f'Not find any project with id: {project_id}!'}), 404
+
+    project['_id'] = str(project['_id'])
+    return jsonify(project)
+
+
+# ---- Task ----
+@app.route('/task/project/<int:project_id>', methods=['POST'])
+def create_task():
+    data = request.get_json()
+
+    if 'task_id' not in data or 'task_name' not in data or 'status' not in data or 'person_name' not in data \
+            or 'project_id' not in data:
+        return jsonify({'message': 'Error, Invalid data'}), 400
+
+    db.task.insert_one(data)
+    return jsonify({'message': 'New task created successfully!'})
+
+
+@app.route('/task/<int:task_id>', methods=['PUT'])
+def update_task(task_id):
+    task = db.task.find_one({'task_id': task_id})
+
+    if task is None:
+        return jsonify({'message': f'Not find any task with id: {task_id}!'}), 404
+
+    data = request.get_json()
+    if 'status' in data: task['status'] = data['status']
+
+    db.project.delete_one({'task_id': task_id})
+    db.project.insert_one(task)
+
+    return jsonify({'message': f'Task with id: {task_id} is updated!'})
+
+
+@app.route('/task/<int:task_id>', methods=['DELETE'])
+def delete_task(task_id):
+    task = db.task.find_one({'task_id': task_id})
+
+    if task is None:
+        return jsonify({'message': f'Not find any task with id: {task_id}!'}), 404
+
+    db.task.delete_one({'task_id': task_id})
+    return jsonify({'message': f'Task with id: {task_id} is deleted!'})
+
+
+@app.route('/tasks', methods=['GET'])
+def view_all_task():
+    tasks = list(db.task.find({}))
+
+    if len(tasks) == 0:
+        return jsonify({'message': 'Not find any task!'}), 404
+
+    for task in tasks:
+        task['_id'] = str(task['_id'])
+
+    return jsonify(tasks)
+
+
+@app.route('/task/<int:task_id>', methods=['GET'])
+def view_taskById(task_id):
+    task = db.task.find_one({'task_id': task_id})
+
+    if task is None:
+        return jsonify({f'message': f'Not find any task with id: {task_id}!'}), 404
+
+    task['_id'] = str(task['_id'])
+    return jsonify(task)
+
+
+@app.route('/task/project/<int:project_id>', methods=['GET'])
+def view_taskByProjectId(project_id):
+    task = db.task.find_one({'project_id': project_id})
+
+    if task is None:
+        return jsonify({f'message': f'Not find any task with project id: {project_id}!'}), 404
+
+    task['_id'] = str(task['_id'])
+    return jsonify(task)
+
+
+# ---- Resource ----
+@app.route('/resource/task/<int:task_id>', methods=['POST'])
+def create_resource():
+    data = request.get_json()
+
+    if 'resource_id' not in data or 'resource_name' not in data or 'description' not in data or 'task_id' not in data:
+        return jsonify({'message': 'Error, Invalid data'}), 400
+
+    db.resource.insert_one(data)
+    return jsonify({'message': 'New resource created successfully!'})
+
+
+@app.route('/resource/<int:resource_id>', methods=['PUT'])
+def update_resource(resource_id):
+    resource = db.resource.find_one({'resource_id': resource_id})
+
+    if resource is None:
+        return jsonify({'message': f'Not find any resource with id: {resource}!'}), 404
+
+    data = request.get_json()
+    if 'resource_name' in data: resource['resource_name'] = data['resource_name']
+    if 'description' in data: resource['description'] = data['description']
+
+    db.resource.delete_one({'resource_id': resource_id})
+    db.resource.insert_one(resource)
+
+    return jsonify({'message': f'Resource with id: {resource_id} is updated!'})
+
+
+@app.route('/resource/<int:resource_id>', methods=['DELETE'])
+def delete_resource(resource_id):
+    resource = db.resource.find_one({'resource_id': resource_id})
+
+    if resource is None:
+        return jsonify({'message': f'Not find any resource with id: {resource_id}!'}), 404
+
+    db.resource.delete_one({'resource_id': resource_id})
+    return jsonify({'message': f'Resource with id: {resource_id} is deleted!'})
+
+
+@app.route('/resources', methods=['GET'])
+def view_all_resource():
+    resources = list(db.resource.find({}))
+
+    if len(resources) == 0:
+        return jsonify({'message': 'Not find any resource!'}), 404
+
+    for resource in resources:
+        resource['_id'] = str(resource['_id'])
+
+    return jsonify(resources)
+
+
+@app.route('/resource/<int:resource_id>', methods=['GET'])
+def view_resourceById(resource_id):
+    resource = db.resource.find_one({'resource_id': resource_id})
+
+    if resource is None:
+        return jsonify({f'message': f'Not find any resource with id: {resource_id}!'}), 404
+
+    resource['_id'] = str(resource['_id'])
+    return jsonify(resource)
+
+
+@app.route('/resource/task/<int:task_id>', methods=['GET'])
+def view_resourceByTaskId(task_id):
+    resource = db.resource.find_one({'task_id': task_id})
+
+    if resource is None:
+        return jsonify({f'message': f'Not find any resource with task id: {task_id}!'}), 404
+
+    resource['_id'] = str(resource['_id'])
+    return jsonify(resource)
 
 
 if __name__ == '__main__':
---
+++ Backend/test_app.py
@@ -0,0 +1,35 @@
+# {
+#     "project_id": 1,
+#     "name": "Project 1",
+#     "status": "Planned",
+#     "start_date": "2022-02-01",
+#     "end_date": "2022-05-01",
+#     "manager_id": "1"
+# }
+# {
+#     "status": "In Progress",
+#     "end_date": "2022-06-01",
+#     "manager_id": "<manager id>"
+# }
+
+# {
+#     "task_id": 1,
+#     "task_name": "Creating homw page",
+#     "status": "In progress",
+#     "person_name": "Ankit",
+#     "project_id": "1"
+# }
+
+# URL: /resource/task/task_id
+# Description: Creates a new resource.
+# Request Body: {
+#     "resource_id": 1,
+#     "resource_name": "image",
+#     "description": "www.image.com",
+#     "task_id": 1
+# }
+
+# {
+#     "resource_name": "Document",
+#     "description": "look the official document"
+# }
